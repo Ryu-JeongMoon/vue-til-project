d9f017618cec8bc1958a46d5806fd4a556e22eb6
jm <rjm9303@gmail.com>
2021-10-20 20:24:48 +0900

    - cookies

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -18,7 +18,6 @@
 </template>
 
 <script>
-import {loginUser} from "@/api";
 import {validateEmail} from "@/utils/validation";
 
 export default {
@@ -47,10 +46,7 @@ export default {
           username: this.username,
           password: this.password,
         }
-        const {data} = await loginUser(userData);
-        console.log(data.token);
-        this.$store.commit('setToken', data.token);
-        this.$store.commit('setUsername', data.user.username);
+        await this.$store.dispatch('LOGIN', userData);
         this.$router.push('/main');
       } catch (error) {
         this.logMessage = `${error.response.data}`
